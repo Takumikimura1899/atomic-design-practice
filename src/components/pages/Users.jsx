c7afd424233ccc9de77cc88f44a7057f1652d15b
Takumikimura1899 <81891074+Takumikimura1899@users.noreply.github.com>
2021-07-12 20:36:20 +0900

    Atomic Design Finish

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `拓-${val}`,
+    image: "https://source.unsplash.com/O5IeTqgkBrU",
+    email: "aaaaaaaaaa",
+    phone: "00000",
+    company: {
+      name: ".....株式会社"
+    },
+    website: "https://www.udemy.com/"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>User一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
